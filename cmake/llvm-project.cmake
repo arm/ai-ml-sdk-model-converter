7ed5a3c91da1231e51862a7740fc3d13c3d8cd5c
Finlay Fletcher <finlay.fletcher@arm.com>
2026-02-03 11:31:30 +0000

    Fix llvm patch bug
    Update the name of the llvm patch in cmake to stop
    error when building with an llvm depedency that
    has already had the patch file applied.
    
    Change-Id: I2971961321d5fa04e8a26644d134299824c2ea23
    Signed-off-by: Finlay Fletcher <finlay.fletcher@arm.com>

--- cmake/llvm-project.cmake
+++ cmake/llvm-project.cmake
@@ -33,7 +33,7 @@ endfunction()
 
 if(EXISTS ${LLVM_PATH}/llvm/CMakeLists.txt)
     if(MODEL_CONVERTER_APPLY_LLVM_PATCH)
-        set(LLVM_PATCH_COMMIT_MESSAGE "llvm-changes-for-model-converter-09-01-2026")
+        set(LLVM_PATCH_COMMIT_MESSAGE "llvm-changes-for-model-converter-30-01-2026")
         execute_process(
             COMMAND git log --grep=${LLVM_PATCH_COMMIT_MESSAGE}
             WORKING_DIRECTORY "${LLVM_PATH}"
